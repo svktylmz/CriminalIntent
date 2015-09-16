59dc41e894272a733071b1ebf101e0d69532a8f1
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-09-16 22:25:37 +0300

    Generic Abstract class used for CrimeActivity class.

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/activities/CrimeActivity.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/activities/CrimeActivity.java
@@ -1,27 +1,12 @@
 package tr.com.hacktusdynamics.android.criminalintent.activities;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
-import tr.com.hacktusdynamics.android.criminalintent.R;
 import tr.com.hacktusdynamics.android.criminalintent.fragments.CrimeFragment;
 
-public class CrimeActivity extends FragmentActivity {
-
+public class CrimeActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if(fragment == null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
-
 }
