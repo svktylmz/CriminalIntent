68e84e58c466ecca6b090ce26c2a8025dfd68ff7
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-09-18 19:29:42 +0300

    ViewHolder handles the custom list item Press event for entire row.

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeListFragment.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeListFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -41,7 +42,7 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mCrimeAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private Crime mCrime;
 
         private TextView mTitleTextView;
@@ -51,6 +52,7 @@ public class CrimeListFragment extends Fragment {
         //constructor
         public CrimeHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_textview);
             mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_date_textview);
@@ -62,6 +64,11 @@ public class CrimeListFragment extends Fragment {
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
             mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_LONG).show();
         }
     }
 
