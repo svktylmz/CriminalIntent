878e54a0315f3a1878cb0f8d3ab380aa206e844a
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-09-18 18:10:35 +0300

    Adapter and ViewHolder implemented for RecyclerView in CrimeListFragment.

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeListFragment.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeListFragment.java
@@ -8,11 +8,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 import tr.com.hacktusdynamics.android.criminalintent.R;
+import tr.com.hacktusdynamics.android.criminalintent.models.Crime;
+import tr.com.hacktusdynamics.android.criminalintent.models.CrimeLab;
 
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mCrimeAdapter;
 
     @Nullable
     @Override
@@ -22,6 +28,63 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView = (RecyclerView) rotView.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
+
         return rotView;
     }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mCrimeAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mCrimeAdapter);
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+        public TextView mTitleTextView;
+
+        //constructor
+        public CrimeHolder(View itemView) {
+            super(itemView);
+            //if itemView is not TextView, program will crash
+            mTitleTextView = (TextView) itemView;
+        }
+
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+        private List<Crime> mCrimes;
+
+        //constructor
+        public CrimeAdapter(List<Crime> mCrimes) {
+            this.mCrimes = mCrimes;
+        }
+
+        /**
+         * Called by RecyclerView when its need a new View to display an item.
+         * You create a View and wrap it in a ViewHolder.
+         */
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CrimeHolder(view);
+        }
+
+        /**
+         * This method will bind a ViewHolder's View to your model object
+         * It receives ViewHolder and a position in your dataset.
+         */
+        @Override
+        public void onBindViewHolder(CrimeHolder crimeHolder, int position) {
+            Crime crime = mCrimes.get(position);
+            crimeHolder.mTitleTextView.setText(crime.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
 }
