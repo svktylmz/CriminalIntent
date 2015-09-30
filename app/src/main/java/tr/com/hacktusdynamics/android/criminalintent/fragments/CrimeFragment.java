0a9df2f277b2aee3757363bbb4f335cd24da46df
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-09-30 21:00:12 +0300

    Sending date to show in DatePicker from CrimeFragment mCrime.date

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeFragment.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeFragment.java
@@ -74,8 +74,8 @@ public class CrimeFragment extends Fragment{
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentManager manager = getFragmentManager();
-                DatePickerFragment dialog = new DatePickerFragment();
+                FragmentManager manager = getActivity().getSupportFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.show(manager, DIALOG_DATE);
             }
         });
