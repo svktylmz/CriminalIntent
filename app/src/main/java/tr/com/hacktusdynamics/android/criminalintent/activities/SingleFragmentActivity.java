ca43fc6ad732df888e67cedce051a0f5a6bec7a3
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-10-01 22:14:31 +0300

    Toolbar created and some menu added to CrimeListFragment.

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/activities/SingleFragmentActivity.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/activities/SingleFragmentActivity.java
@@ -2,12 +2,12 @@ package tr.com.hacktusdynamics.android.criminalintent.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import tr.com.hacktusdynamics.android.criminalintent.R;
 
-public abstract class SingleFragmentActivity extends FragmentActivity{
+public abstract class SingleFragmentActivity extends AppCompatActivity{
 
     protected abstract Fragment createFragment();
 
