e4f5d0f38009d912631805db98feb9b66ca22b14
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-10-03 21:39:11 +0300

    String Format.

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeFragment.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeFragment.java
@@ -119,4 +119,28 @@ public class CrimeFragment extends Fragment{
         super.onPause();
         CrimeLab.get(getActivity()).updateCrime(mCrime);
     }
+
+    private String getCrimeReport(){
+        String solvedString = null;
+        if(mCrime.isSolved()){
+            solvedString = getString(R.string.crime_report_solved);
+        }else {
+            solvedString = getString(R.string.crime_report_unsolved);
+        }
+        String dateFormat = "EEE, MMM dd";
+        String dateString = android.text.format.DateFormat.format(dateFormat, mCrime.getDate()).toString();
+        String suspect = mCrime.getSuspect();
+        if(suspect == null){
+            suspect = getString(R.string.crime_report_no_suspect);
+        }else {
+            suspect = getString(R.string.crime_report_suspect, suspect);
+        }
+        String report = getString(R.string.crime_report,
+                mCrime.getTitle(),
+                dateString,
+                solvedString,
+                suspect);
+
+        return report;
+    }
 }
