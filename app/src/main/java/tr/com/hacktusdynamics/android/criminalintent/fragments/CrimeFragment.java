439ccff964d682b151c20aa6e4a8f85aedb437ff
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-10-03 22:16:40 +0300

    Sending crime report with created action chooser.

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeFragment.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/fragments/CrimeFragment.java
@@ -103,6 +103,7 @@ public class CrimeFragment extends Fragment{
                 intent.setType("text/plain");
                 intent.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
                 intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+                intent = Intent.createChooser(intent, getString(R.string.send_report));
                 startActivity(intent);
             }
         });
