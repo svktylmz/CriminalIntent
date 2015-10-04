5a57f6c5293ee24b1c255b1c3e1fc8caf7a95ff9
svktylmz <sevketyilmaz@iyte.edu.tr>
2015-10-04 13:20:12 +0300

    activity_twopane added and hooked with CrimeListActivity.

--- app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/activities/CrimeListActivity.java
+++ app/src/main/java/tr/com/hacktusdynamics/android/criminalintent/activities/CrimeListActivity.java
@@ -2,11 +2,17 @@ package tr.com.hacktusdynamics.android.criminalintent.activities;
 
 import android.support.v4.app.Fragment;
 
+import tr.com.hacktusdynamics.android.criminalintent.R;
 import tr.com.hacktusdynamics.android.criminalintent.fragments.CrimeListFragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_twopane;
     }
 }
